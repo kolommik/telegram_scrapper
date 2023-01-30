2fcbc793bc8cf130d40425603807ba2f3a5949bb
kolommik <mikhail.kolomasov@gmail.com>
2023-01-30 21:20:34 +0300

    add sample env variables

---
+++ add_env.sample.ps1
@@ -0,0 +1,3 @@
+[Environment]::SetEnvironmentVariable("TELEGRAM_API_ID", "123456", [System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("TELEGRAM_API_HASH", "0123456789abcdef0123456789abcde2", [System.EnvironmentVariableTarget]::Machine)
+
